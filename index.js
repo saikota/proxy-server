c156f71e6c9f0146e0f7bcd3b028227fd4c2c193
Sai Kota Mac <sai.kota@hotmail.com>
2015-04-24 16:33:24 -0700

    working version of the proxy server.

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
 let http = require('http')
 let request = require('request')
-let destinationUrl = 'http://127.0.0.1:8000'
+let fs = require('fs')
+let through = require('through')
+//let destinationUrl = 'http://127.0.0.1:8000'
+
+
+let argv = require('yargs')
+           .default('host','127.0.0.1')
+           .argv
+
+let scheme = 'http://'
+let port =argv.port || argv.host === '127.0.0.1'? 8000:80
+let destinationUrl = argv.url || scheme + argv.host + ':' + port
+let logStream = argv.mylog ? fs.createWriteStream(argv.mylog): process.stdout
+console.log('argv2',argv.mylog)
+
 
 http.createServer((req, res) => {
-    // console.log(`Request received at: ${req.url}`)
-    // res.end('hello world\n')
+    logStream.write("\n Echo Request:" +JSON.stringify(req.headers))
     for(let header in req.headers){
-    	res.setHeader(header,req.headers[header]);
+    	res.setHeader(header,req.headers[header])
     }
+    req.pipe(process.stdout)
+    through(req,logStream,{autoDestroy:false})
     req.pipe(res);
 }).listen(8000)
 
-console.log(" we are listening on http://127.0.0.1:8000 ")
+logStream.write(" we are listening on http://127.0.0.1:8000 ")
 
 http.createServer((req, res) => {
+	let url = destinationUrl
+	if(req.headers['x-destination-url']){
+		url = req.headers['x-destination-url']
+	}
    let options ={
    	headers:req.headers,
-   	url:destinationUrl
+   	url:url + req.url
    }
+   logStream.write("\n proxy request" + JSON.stringify(req.headers))
+   through(req,logStream,{autoDestroy:false})
+
   let destinationResponse = req.pipe(request(options))
   destinationResponse.pipe(res)
+   through(destinationResponse,logStream,{autoDestroy:false})
 }).listen(8001)
