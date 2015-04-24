984037498d53311ed1a41ff8de747e9210b8b20c
Sai Kota Mac <sai.kota@hotmail.com>
2015-04-24 09:06:36 -0700

    Request Module

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let http = require('http')
+let request = require('request')
+let destinationUrl = 'http://127.0.0.1:8000'
 
 http.createServer((req, res) => {
     // console.log(`Request received at: ${req.url}`)
@@ -10,3 +12,12 @@ http.createServer((req, res) => {
 }).listen(8000)
 
 console.log(" we are listening on http://127.0.0.1:8000 ")
+
+http.createServer((req, res) => {
+   let options ={
+   	headers:req.headers,
+   	url:destinationUrl
+   }
+  let destinationResponse = req.pipe(request(options))
+  destinationResponse.pipe(res)
+}).listen(8001)
